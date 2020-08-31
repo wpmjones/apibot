7a58317a9785545611098669ee32ea9a61d19d75
W. Patrick Jones <wpmjones@gmail.com>
2020-08-31 09:21:42 -0700

    add commands for FAQ

--- cogs/general.py
+++ cogs/general.py
@@ -5,6 +5,27 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="regex")
+    async def regex(self, ctx):
+        """Responds with the RegEx for player/clan tags"""
+        await ctx.send("^#[PYLQGRJCUV0289]+$")
+
+    @commands.command(name="rate_limit")
+    async def rate_limit(self, ctx):
+        """Responds with the rate limit informations"""
+        await ctx.send("We have found that the approximate rate limit is 30-40 requests per second. Staying "
+                       "below this should be safe.")
+
+    @commands.command(name="vps")
+    async def vps(self, ctx):
+        """Responds with a link to a Dsicord message on inexpensive VPS options"""
+        await ctx.send("https://discordapp.com/channels/566451504332931073/566451504903618561/662484243808780309")
+
+    @commands.command(name="links")
+    async def links(self, ctx):
+        """Responds with a link to a Discord message on the Discord Link API (by ReverendMike)"""
+        await ctx.send("https://discordapp.com/channels/566451504332931073/681617252814159904/685533375280578610")
+
 
 def setup(bot):
     bot.add_cog(General(bot))
