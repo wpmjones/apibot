c49b20c53778e7b41bc23665f07b7bf18f846549
Patrick <wpmjones@gmail.com>
2020-09-13 16:21:15 -0700

    add commands to add, list, and toggle monitoring bots

--- cogs/downtime.py
+++ cogs/downtime.py
@@ -19,11 +19,12 @@ def to_time(seconds):
 
 
 class Bot:
-    def __init__(self, name, bot_id, channel_id, owner):
+    def __init__(self, name, bot_id, channel_id, owner, monitor):
         self.name = name
         self.bot_id = bot_id
         self.channel_id = channel_id
         self.owner = owner
+        self.monitor = monitor
 
     async def notify_down(self, bot):
         """Notify bot owner that the bot is down"""
@@ -45,52 +46,164 @@ class Bot:
 class Downtime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bots = []
         self.watchman.start()
 
     def cog_unload(self):
         self.watchman.cancel()
 
-    bots = [
-        Bot("RCS Bot", settings['bots']['rcs'], 568167391662440448, 251150854571163648),
-        Bot("The Arborist", settings['bots']['oak'], settings['channels']['mod-log'], 251150854571163648),
-        Bot("Clash Recruiter", settings['bots']['cr'], 662284520329969677, 581442925611712513),
-        Bot("Donation Tracker", settings['bots']['dt'], 594280479881035776, 230214242618441728),
-        Bot("Bat", settings['bots']['bat'], 647839792066723840, 304690705996054528),
-        Bot("Karen", settings['bots']['karen'], 647839792066723840, 304690705996054528),
-        Bot("Maniacs Bot", settings['bots']['maniacs'], 568511927597400074, 325493699478028290),
-        Bot("Minion Bot", settings['bots']['minion'], 567551819451072541, 267057699856842753),
-        # Bot("Wiz Bot", settings['bots']['wiz'], 568162367385501722, 243934463615041536),
-        Bot("WarMatch", settings['bots']['wm'], 567551904071155732, 126084695824924673),
-        Bot("ClashCord", settings['bots']['clashcord'], 608672949968437258, 239810643581075457),
-        Bot("Sidekick", settings['bots']['sk'], 567550343748124682, 134153092378656769),
-        Bot("ClashPerk", settings['bots']['clashperk'], 691885320035237940, 444432489818357760),
-        Bot("Ivory", settings['bots']['ivory'], 636222832430284821, 294035020236980224),
-        Bot("Ruby", settings['bots']['ruby'], 636222832430284821, 294035020236980224),
-        Bot("Alph4", settings['bots']['alph4'], 715903895741661194, 434693228189712385),
-        # Bot("ScatterBot", settings['bots']['scatter'], 695448751741075488, 246286410946969610),
-    ]
+    async def init_bots(self):
+        """Initialize self.bots with info from database"""
+        self.bots = []  # clear list of any previous entries
+        sql = "SELECT bot_id, name, owner_id, channel_id, monitor FROM bot_owners ORDER BY name"
+        fetch = await self.bot.pool.fetch(sql)
+        for row in fetch:
+            self.bots.append(Bot(row['name'], row['bot_id'], row['channel_id'], row['owner_id'], row['monitor']))
+
+    @commands.group(name="bot", invoke_without_command=True)
+    async def my_bot(self, ctx):
+        if ctx.invoked_subcommand is not None:
+            return
+        response = ("To list bots, use `/bot list`\n"
+                    "To add a bot, use `/bot add [bot_id]`\n"
+                    "To toggle monitoring, use `/bot monitor [bot_id]`\n"
+                    "To delete a bot, use `/bot remove [bot_id]` - FUTURE ADDITION")
+        await ctx.send(response)
+
+    @my_bot.command(name="add")
+    async def my_bot_add(self, ctx, user: discord.User = None):
+        """Add a bot to be monitored.  Provide the Discord ID or mention the bot and you will be prompted for other
+        information.
+
+        **Examples:**
+        /bot add 123457890
+        /bot add @Minion Bot
+
+        **Other Info:**
+        You will be prompted to provide the Discord ID of the bot owner and the channel where notifications will be
+        sent."""
+        if not user:
+            return await ctx.send("Please mention or provide a valid Discord ID for the bot.")
+        if not user.bot:
+            return await ctx.send(f"It would appear that {user.name} ({user.id}) is not a bot user.")
+
+        def check_author(m):
+            return m.author == ctx.author
+
+        # ask user for bot owner ID
+        try:
+            await ctx.send("Please mention or enter Discord ID of the bot owner (this is the person I will "
+                           "notify when the bot goes offline).")
+            response = await ctx.bot.wait_for("message", check=check_author, timeout=60)
+        except asyncio.TimeoutError:
+            return await ctx.send("Seriously, I'm not going to wait that long. Start over!")
+        try:
+            if type(response.content) == discord.Member:
+                owner = response
+            else:
+                guild = self.bot.get_guild(settings['guild']['junkies'])
+                owner = guild.get_member(int(response.content))
+        except ValueError:
+            return await ctx.send(f"{response.content} is not a valid Discord ID.  Please start over and try again.")
+        if not owner:
+            return await ctx.send(f"{response.content} is not a valid Discord member of the COC API Junkies server.  "
+                                  f"Please start over and try again.")
+        if owner.bot:
+            return await ctx.send(f"{owner.name} ({owner.id}) is a bot and cannot be added as a bot owner.")
+
+        # ask user for channel id (for reporting)
+        try:
+            await ctx.send("Please mention or enter the Discord ID of the channel in which you would like me "
+                           "to report outages.")
+            response = await ctx.bot.wait_for("message", check=check_author, timeout=60)
+        except asyncio.TimeoutError:
+            return await ctx.send("I waited 60 seconds for you to talk to me. Now I'm ignoring you!")
+        try:
+            if type(response.content) == discord.TextChannel:
+                channel = response
+            else:
+                channel = self.bot.get_channel(int(response.content))
+        except ValueError:
+            return await ctx.send(f"{response.content} is not a valid Discord ID.  Please start over and try again.")
+        if not channel:
+            return await ctx.send(f"{response.content} is not a valid Discord channel.  Please start over and try again.")
+        if channel.guild.id != settings['guild']['junkies']:
+            return await ctx.send(f"{channel.name} ({channel.id}) is not a valid channel on COC API Junkies.  "
+                                  f"I am only able to report things on that server.  Please start over and try "
+                                  f"again.")
+
+        sql = ("INSERT INTO bot_owners (bot_id, name, owner_id, channel_id) "
+               "VALUES ($1, $2, $3, $4)")
+        await self.bot.pool.execute(sql, user.id, user.name, owner.id, channel.id)
+        await ctx.send(f"Congratulations! You have successfully added {user.name} to the bot monitoring system. "
+                       f"If there is an outage that lasts more than 60 seconds, I will ping {owner.display_name} in "
+                       f"{channel.mention}.  To toggle monitoring, please use `/bot monitor {user.id}` or "
+                       f"`/bot monitor @{user.display_name}#{user.discriminator}`.")
+
+    @my_bot.command(name="list")
+    async def my_bot_list(self, ctx):
+        """List the bots that are being monitored.
+
+        **Example:**
+        /bot list"""
+        await self.init_bots()
+        embed = discord.Embed(title="Bots that are configured for monitoring")
+        for bot in self.bots:
+            owner = self.bot.get_user(bot.owner)
+            channel = self.bot.get_channel(bot.channel_id)
+            embed.add_field(name=f"{bot.name}",
+                            value=f"Bot ID: {bot.bot_id}\n"
+                                  f"Owner: {owner.mention}\n"
+                                  f"Channel: {channel.mention}\n"
+                                  f"Monitoring: {bot.monitor}",
+                            inline=True)
+        await ctx.send(embed=embed)
+
+    @my_bot.command(name="monitor")
+    async def my_bot_monitor(self, ctx, bot: discord.Member = None):
+        """Toggle monitoring for the specified bot
+
+        **Example:**
+        /bot monitor 1234567890
+        /bot monitor @Ruby
+        """
+        if not bot:
+            return await ctx.send("Please provide a Discord ID or mention the bot in question.")
+        sql = ("UPDATE bot_owners "
+               "SET monitor = NOT monitor "
+               "WHERE bot_id = $1 "
+               "RETURNING monitor")
+        monitor = await self.bot.pool.fetchval(sql, bot.id)
+        if monitor:
+            new_monitor = "ON"
+        else:
+            new_monitor = "OFF"
+        await ctx.send(f"Monitoring for {bot.display_name} is now set to {new_monitor}.")
 
     @commands.Cog.listener()
     async def on_member_update(self, before, member):
         """Task for monitoring API bots
         Downtime is stored in the bot_downtime table of postgresql
         """
+        conn = self.bot.pool
+        # Only monitor updates seen in the Junkies server
         if member.guild.id != settings['guild']['junkies']:
             return
-        if member.id not in [bot.bot_id for bot in self.bots]:
+        # Is this user a bot?
+        if not member.bot:
             return
+        # Is this update a status change?
         if before.status == member.status:
             return
-        # Kinda wish there were a better way to pull the bot object out of the list
-        bot = None
-        for _ in self.bots:
-            if _.bot_id == member.id:
-                bot = _
-                break
-        if not bot:  # Shouldn't happen, but if it does...
-            channel = await self.bot.get_channel(settings['channels']['mod-log'])
-            return await channel.send(f"`on_member_update`: No bot found in list - {member.name} ({member.id})")
-        conn = self.bot.pool
+        sql = "SELECT bot_id, name, owner_id, channel_id, monitor FROM bot_owners WHERE bot_id = $1"
+        row = await conn.fetchrow(sql, member.id)
+        if not row:
+            return
+        bot = Bot(row['name'], row['bot_id'], row['channel_id'], row['owner_id'], row['monitor'])
+        self.bot.logger.info(f"Bot found in db: {bot.name}.  Monitoring: {bot.monitor}")
+        # Are we currently monitoring this bot?
+        if not bot.monitor:
+            return
         now = datetime.utcnow()
         offline_sql = "SELECT offline_start FROM bot_downtime WHERE online = False AND bot_id = $1"
         reported_sql = ("UPDATE bot_downtime "
@@ -139,7 +252,10 @@ class Downtime(commands.Cog):
         now = datetime.utcnow()
         update_sql = "UPDATE bot_downtime SET last_notification = $1 WHERE bot_id = $2 AND online = False"
         sql = "SELECT offline_start, last_notification FROM bot_downtime WHERE bot_id = $1 AND online = False"
+        await self.init_bots()
         for bot in self.bots:
+            if not bot.monitor:
+                continue
             fetch = await conn.fetchrow(sql, bot.bot_id)
             if not fetch:
                 continue
