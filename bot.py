fcd55bb5ae4ee4e9d2aa02700f3ded2813575f53
Patrick <wpmjones@gmail.com>
2021-03-29 12:51:00 -0700

    change prefix to //

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ initial_extensions = [
 
 if enviro == "LIVE":
     token = settings['discord']['token']
-    prefix = "/"
+    prefix = "//"
     log_level = "INFO"
     coc_names = "galaxy"
     # append to initial_extensions if additional cogs are desired
