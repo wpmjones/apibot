622f372f1c6538cc702d5d16693986149ff41808
Patrick <wpmjones@gmail.com>
2021-03-29 16:05:45 -0700

    change to // prefix

--- cogs/members.py
+++ cogs/members.py
@@ -42,7 +42,7 @@ class MembersCog(commands.Cog):
         else:
             channel = self.bot.get_channel(settings['channels']['admin'])
             await channel.send(f"{member.mention} has just been invited to the server. "
-                               f"Perhaps it is time to set up a demo channel?  Try `/setup {member.mention} @owner`")
+                               f"Perhaps it is time to set up a demo channel?  Try `//setup {member.mention} @owner`")
         mod_log = self.bot.get_channel(settings['channels']['mod-log'])
         msg = f"{member.display_name}#{member.discriminator} just joined the server."
         await mod_log.send(msg)
