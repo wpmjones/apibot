59660ce693b4a01fb311dc579d0145c8964b3abb
Patrick <wpmjones@gmail.com>
2021-09-11 13:15:59 -0700

    tweaking welcome command

--- cogs/members.py
+++ cogs/members.py
@@ -10,11 +10,8 @@ class MembersCog(commands.Cog):
         self.bot = bot
 
     @commands.command(name="welcome", hidden=True)
-    async def welcome(self, ctx, discord_id):
+    async def welcome(self, ctx, member: discord.Member = None):
         guild = self.bot.get_guild(settings['guild']['junkies'])
-        print(guild)
-        member = guild.get_member(discord_id)
-        print(member.name)
         if not member:
             return await ctx.send("Member does not exist.")
         channel = self.bot.get_channel(settings['channels']['welcome'])
