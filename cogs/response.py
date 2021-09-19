52f4f8458d21b18ee940473b44d7076f903bce8b
Patrick <wpmjones@gmail.com>
2021-09-19 15:07:21 -0700

    before loop, wait for bot

--- cogs/response.py
+++ cogs/response.py
@@ -196,6 +196,10 @@ class Response(commands.Cog):
         except:
             self.bot.logger.exception("Channel update failed")
 
+    @response_check.before_loop
+    async def before_response_check(self):
+        await self.bot.wait_until_ready()
+
 
 def setup(bot):
     bot.add_cog(Response(bot))
