5fe3709c88728f8402d19b19453aeb51c5b55606
tammes <tabu.der.erste@gmx.de>
2022-08-19 10:39:06 +0200

    convert bot command group to slash

--- cogs/downtime.py
+++ cogs/downtime.py
@@ -7,6 +7,14 @@ from datetime import datetime, timedelta
 from nextcord.ext import commands, tasks
 from config import settings
 
+enviro = settings['enviro']
+
+JUNKIES_GUILD_ID = settings['guild']['junkies']
+if enviro == "LIVE":
+    GUILD_IDS = [JUNKIES_GUILD_ID, settings['guild']['bot_logs']]
+else:
+    GUILD_IDS = [settings['guild']['bot_logs']]
+
 
 def to_time(seconds):
     d, r = divmod(seconds, 86400)
@@ -76,129 +84,80 @@ class Downtime(commands.Cog):
             self.bots.append(Bot(row['name'], row['bot_id'], row['channel_id'], row['owner_id'],
                                  row['monitor'], row['email']))
 
-    @commands.group(name="bot", invoke_without_command=True)
-    async def my_bot(self, ctx):
-        if ctx.invoked_subcommand is not None:
-            return
-        response = ("To list bots, use `//bot list`\n"
-                    "To add a bot, use `//bot add [bot_id]`\n"
-                    "To toggle monitoring, use `//bot monitor [bot_id]`\n"
-                    "To delete a bot, use `//bot remove [bot_id]` - FUTURE ADDITION")
-        await ctx.send(response)
+    @nextcord.slash_command(name="bot", description="Command group to manage demo bots", guild_ids=GUILD_IDS)
+    @commands.has_role("Admin")
+    async def my_bot(self, interaction: nextcord.Interaction):
+        """ This is just for the slash command group and will never get called on its own, so we can pass. """
+        pass
 
-    @my_bot.command(name="test", hidden=True)
-    async def my_bot_test(self, ctx, member: nextcord.Member = None):
-        if not member:
-            return await ctx.send("Gimme a bot man")
+    @my_bot.subcommand(name="test", description="Tests the downtime notification")
+    async def my_bot_test(self, interaction: nextcord.Interaction, member: nextcord.Member):
+        """ Tests the downtime notifier for a given bot. """
         self.bot.logger.debug("Starting...")
         sql = "SELECT bot_id, name, owner_id, channel_id, monitor, email FROM bot_owners WHERE bot_id = $1"
         row = await self.bot.pool.fetchrow(sql, member.id)
         if not row:
+            await interaction.response.send_message("This bot is not in my DB.", ephemeral=True)
             return
         bot = Bot(row['name'], row['bot_id'], row['channel_id'], row['owner_id'], row['monitor'], row['email'])
         self.bot.logger.debug("About to send notification")
         await bot.notify_down(self.bot)
+        await interaction.response.send_message("Test done.", ephemeral=True)
 
-    @my_bot.command(name="add")
-    async def my_bot_add(self, ctx, user: nextcord.User = None):
-        """Add a bot to be monitored.  Provide the Discord ID or mention the bot and you will be prompted for other
-        information.
-
-        **Examples:**
-        //bot add 123457890
-        //bot add @Minion Bot
-
-        **Other Info:**
-        You will be prompted to provide the Discord ID of the bot owner and the channel where notifications will be
-        sent."""
-        if not user:
-            return await ctx.send("Please mention or provide a valid Discord ID for the bot.")
-        if not user.bot:
-            return await ctx.send(f"It would appear that {user.name} ({user.id}) is not a bot user.")
-
-        def check_author(m):
-            return m.author == ctx.author
-
-        # ask user for bot owner ID
-        try:
-            await ctx.send("Please mention or enter Discord ID of the bot owner (this is the person I will "
-                           "notify when the bot goes offline).")
-            response = await ctx.bot.wait_for("message", check=check_author, timeout=60)
-        except asyncio.TimeoutError:
-            return await ctx.send("Seriously, I'm not going to wait that long. Start over!")
-        try:
-            if type(response.content) == nextcord.Member:
-                owner = response
-            else:
-                guild = self.bot.get_guild(settings['guild']['junkies'])
-                owner = guild.get_member(int(response.content))
-        except ValueError:
-            return await ctx.send(f"{response.content} is not a valid Discord ID.  Please start over and try again.")
-        if not owner:
-            return await ctx.send(f"{response.content} is not a valid Discord member of the Clash API Developers"
-                                  f" server.  Please start over and try again.")
+    @my_bot.subcommand(name="add", description="Add a bot to be monitored")
+    async def my_bot_add(self, interaction: nextcord.Interaction,
+                         bot: nextcord.User,
+                         owner: nextcord.User,
+                         channel: nextcord.TextChannel):
+        """ Ads a given bot with a given owner and its demo channel to the database for future monitoring. """
+        if not bot.bot:
+            return await interaction.response.send_message(f"It would appear that {bot.name} ({bot.id}) is not a "
+                                                           f"bot user.")
         if owner.bot:
-            return await ctx.send(f"{owner.name} ({owner.id}) is a bot and cannot be added as a bot owner.")
-
-        # ask user for channel id (for reporting)
-        try:
-            await ctx.send("Please mention or enter the Discord ID of the channel in which you would like me "
-                           "to report outages.")
-            response = await ctx.bot.wait_for("message", check=check_author, timeout=60)
-        except asyncio.TimeoutError:
-            return await ctx.send("I waited 60 seconds for you to talk to me. Now I'm ignoring you!")
-        try:
-            if type(response.content) == nextcord.TextChannel:
-                channel = response
-            else:
-                channel = self.bot.get_channel(int(response.content))
-        except ValueError:
-            return await ctx.send(f"{response.content} is not a valid Discord ID.  Please start over and try again.")
-        if not channel:
-            return await ctx.send(f"{response.content} is not a valid Discord channel.  Please start over and try again.")
-        if channel.guild.id != settings['guild']['junkies']:
-            return await ctx.send(f"{channel.name} ({channel.id}) is not a valid channel on Clash API Developers.  "
-                                  f"I am only able to report things on that server.  Please start over and try "
-                                  f"again.")
+            return await interaction.response.send_message(f"{owner.name} ({owner.id}) is a bot and cannot be added "
+                                                           f"as a bot owner.")
 
         sql = ("INSERT INTO bot_owners (bot_id, name, owner_id, channel_id) "
                "VALUES ($1, $2, $3, $4)")
-        await self.bot.pool.execute(sql, user.id, user.name, owner.id, channel.id)
-        await ctx.send(f"Congratulations! You have successfully added {user.name} to the bot monitoring system. "
-                       f"If there is an outage that lasts more than 60 seconds, I will ping {owner.display_name} in "
-                       f"{channel.mention}.  To toggle monitoring, please use `//bot monitor {user.id}` or "
-                       f"`//bot monitor @{user.display_name}#{user.discriminator}`.")
+        await self.bot.pool.execute(sql, bot.id, bot.name, owner.id, channel.id)
+        await interaction.response.send_message(f"Congratulations! You have successfully added {bot.name} to the bot "
+                                                f"monitoring system. If there is an outage that lasts more than 60 "
+                                                f"seconds, I will ping {owner.display_name} in {channel.mention}.  To "
+                                                f"toggle monitoring, please use `//bot monitor {bot.id}` or "
+                                                f"`//bot monitor @{bot.display_name}#{bot.discriminator}`.")
 
-    @my_bot.command(name="list")
-    async def my_bot_list(self, ctx):
+    @my_bot.subcommand(name="list")
+    async def my_bot_list(self, interaction: nextcord.Interaction):
         """List the bots that are being monitored.
 
         **Example:**
-        //bot list"""
-        async with ctx.typing():
-            await self.init_bots()
-            embed = nextcord.Embed(title="Bots that are configured for monitoring")
-            for bot in self.bots:
-                owner = self.bot.get_user(bot.owner)
-                channel = self.bot.get_channel(bot.channel_id)
-                embed.add_field(name=f"{bot.name}",
-                                value=f"Bot ID: {bot.bot_id}\n"
-                                      f"Owner: {owner.mention}\n"
-                                      f"Channel: {channel.mention}\n"
-                                      f"Monitoring: {bot.monitor}",
-                                inline=True)
-            await ctx.send(embed=embed)
+        /bot list"""
+        await interaction.response.defer()
+        await self.init_bots()
+        embeds = []
+        for i, bot in enumerate(self.bots):
+            if i % 24 == 0:
+                embeds.append(nextcord.Embed(title="Bots that are configured for monitoring"))
+            owner = self.bot.get_user(bot.owner)
+            channel = self.bot.get_channel(bot.channel_id)
+            embeds[-1].add_field(name=f"{bot.name}",
+                                 value=f"Bot ID: {bot.bot_id}\n"
+                                       f"Owner: {owner.mention}\n"
+                                       f"Channel: {channel.mention}\n"
+                                       f"Monitoring: {bot.monitor}",
+                                 inline=True)
+        if len(embeds) > 1:
+            for i, embed in enumerate(embeds):
+                embed.title += f" ({i})"
+        await interaction.response.send_message(embeds=embeds)
 
-    @my_bot.command(name="monitor")
-    async def my_bot_monitor(self, ctx, bot: nextcord.Member = None):
+    @my_bot.subcommand(name="monitor")
+    async def my_bot_monitor(self, interaction: nextcord.Interaction, bot: nextcord.Member):
         """Toggle monitoring for the specified bot
 
         **Example:**
-        //bot monitor 1234567890
-        //bot monitor @Ruby
+        /bot monitor @Ruby
         """
-        if not bot:
-            return await ctx.send("Please provide a Discord ID or mention the bot in question.")
         sql = ("UPDATE bot_owners "
                "SET monitor = NOT monitor "
                "WHERE bot_id = $1 "
@@ -208,7 +167,7 @@ class Downtime(commands.Cog):
             new_monitor = "ON"
         else:
             new_monitor = "OFF"
-        await ctx.send(f"Monitoring for {bot.display_name} is now set to {new_monitor}.")
+        await interaction.response.send_message(f"Monitoring for {bot.display_name} is now set to {new_monitor}.")
 
     @commands.Cog.listener()
     async def on_guild_channel_delete(self, channel):
