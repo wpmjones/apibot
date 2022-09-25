492cc457ef024f96e59738964a88e1299998e68e
Patrick <wpmjones@gmail.com>
2022-09-24 19:25:37 -0700

    doobie warning

--- cogs/general.py
+++ cogs/general.py
@@ -256,7 +256,7 @@ class General(commands.Cog):
 
     @commands.command(name="xtest")
     async def create_welcome(self, ctx):
-        """Recreate the welcome message for new members"""
+        """Doobie, don't run this command in #admin. No bueno"""
         # channel = self.bot.get_channel(WELCOME_CHANNEL_ID)
         channel = ctx.channel
         await channel.purge()
@@ -553,7 +553,7 @@ class General(commands.Cog):
                                       f"This message will self-destruct in 120 seconds.",
                                       delete_after=120.0)
 
-    @nextcord.slash_command(name="clear", guild_ids=GUILD_IDS)
+    @nextcord.slash_command(name="xc", guild_ids=GUILD_IDS)
     @application_checks.has_role("Admin")
     async def clear(self, interaction: nextcord.Interaction, msg_count: int = None):
         """Clears the specified number of messages in the current channel (defaults to all messages).
